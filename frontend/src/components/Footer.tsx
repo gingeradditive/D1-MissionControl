773fcedd36e2bc40c182f3d0c2aa0e4b63a81536
Giacomo Guaresi <guaresi.giacomo@gmail.com>
2025-07-21 00:55:12 +0200

    feat: enhance Footer component with styled switch and improved layout

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,8 +1,44 @@
-import { Box, IconButton, Typography, Switch } from '@mui/material';
+import React, { useState } from "react";
+import { Box, IconButton, Typography, Switch, styled } from '@mui/material';
 import ThermostatIcon from '@mui/icons-material/Thermostat';
 import WaterDropIcon from '@mui/icons-material/WaterDrop';
+import PowerSettingsNewIcon from "@mui/icons-material/PowerSettingsNew";
+
+const StyledSwitch = styled(Switch)(({ theme }) => ({
+  width: 56.5,
+  height: 34.5,
+  padding: 7,
+  transform: "scale(1.5)",
+  "& .MuiSwitch-switchBase": {
+    padding: 8,
+    "&.Mui-checked": {
+      transform: "translateX(22px)",
+      color: "#fff",
+      "& + .MuiSwitch-track": {
+        backgroundColor: "green",
+        opacity: 1,
+      },
+    },
+  },
+  "& .MuiSwitch-thumb": {
+    backgroundColor: "#f1f1f1",
+    width: 18,
+    height: 18,
+  },
+  "& .MuiSwitch-track": {
+    borderRadius: 20 / 2,
+    backgroundColor: "red",
+    opacity: 1,
+  },
+}));
 
 export default function Footer() {
+  const [checked, setChecked] = useState(false);
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
+
   return (
     <Box
       display="flex"
@@ -14,14 +50,27 @@ export default function Footer() {
       mt={2}
     >
       <Box display="flex" justifyContent="center" alignItems="center">
-      <ThermostatIcon fontSize="small" />
-      <Typography variant="h6">24°</Typography>
-      <Typography variant="h6" sx={{ color: "#cccccc", padding: "0px 10px" }} >/</Typography>
-      <WaterDropIcon fontSize="small" />
-      <Typography variant="h6">10%</Typography>
+        <ThermostatIcon fontSize="small" />
+        <Typography variant="h6">24°</Typography>
+        <Typography variant="h6" sx={{ color: "#cccccc", padding: "0px 10px" }} >/</Typography>
+        <WaterDropIcon fontSize="small" />
+        <Typography variant="h6">10%</Typography>
       </Box>
-      <Box display="flex" justifyContent="end" alignItems="center">
-        <Switch />
+      <Box position="relative" display="flex" justifyContent="end" alignItems="center">
+        <StyledSwitch checked={checked} onChange={handleChange} />
+        <Box
+          position="absolute"
+          left={checked ? 37 : 4.5}
+          top={10}
+          pointerEvents="none"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <PowerSettingsNewIcon
+            style={{ fontSize: 14, color: "gray" }}
+          />
+        </Box>
       </Box>
     </Box>
   );
