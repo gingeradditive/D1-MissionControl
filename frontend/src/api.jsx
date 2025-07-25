6ed42c5cf23f3e6ad6490c01c1da55c422c60ad7
Giacomo Guaresi <guaresi.giacomo@gmail.com>
2025-07-25 10:15:18 +0200

    feat: aggiornare l'URL di base per l'apiClient utilizzando il protocollo e l'hostname correnti

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+const BASE_URL = `${window.location.protocol}//${window.location.hostname}:8000`;
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
