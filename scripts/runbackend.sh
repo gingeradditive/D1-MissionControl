050e96fd7b9ded9c2a9cc09e0e40647324516e5f
Giacomo Guaresi <guaresi.giacomo@gmail.com>
2025-08-06 11:55:30 +0200

    chore: add script for debug backend

--- scripts/runbackend.sh
+++ scripts/runbackend.sh
@@ -9,8 +9,8 @@ cd ..
 echo "🛑 Arresto del servizio dryer-backend..."
 sudo systemctl stop dryer-backend.service
 
-echo "🐍 Creazione di un ambiente virtuale..."
-python3 -m venv venv
+echo "🐍 Attivazione ambiente virtuale..."
+source venv/bin/activate
 
 echo "🚀 Avvio del server FastAPI in modalità debug..."
-venv/bin/python -m uvicorn backend.api.main:app --host 0.0.0.0 --port 8000
+python -m uvicorn backend.api.main:app --host 0.0.0.0 --port 8000
